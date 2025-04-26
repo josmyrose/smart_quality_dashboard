a062986760f607be7b8f5acb85a3baf1af096a66
josmyrose <josmyrose@gmail.com>
2025-04-26 11:38:14 +0200

    main file updated for running the app without  actually running model training during tests

--- app/model.py
+++ app/model.py
@@ -1,5 +1,10 @@
+import os
 import joblib
 
 def load_model():
-    return joblib.load('model.pkl')
-
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        # Running in GitHub Actions — return a dummy model
+        return None
+    else:
+        # Running locally — load the real model
+        return joblib.load('model.pkl')
