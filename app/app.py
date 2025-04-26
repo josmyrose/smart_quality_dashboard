ff3c7d4a5ded59d5c8349b18f711c98577aa801c
josmyrose <josmyrose@gmail.com>
2025-04-26 18:01:45 +0200

    main file is renamed as app

--- app/app.py
+++ app/app.py
@@ -2,18 +2,17 @@
 from flask import Flask, request, jsonify
 from model import load_model
 from utils import predict_defect
-from database import load_sensor_data, load_defect_data, load_maintenance_data
+from database import load_maintenance_data
 from utils import preprocess_sensor_data, train_model
 from flask import Flask, render_template
-
+from database import load_data
 app = Flask(__name__)
 
 # Load and preprocess data
 #sensor_data = preprocess_sensor_data(load_sensor_data())
 #defect_data = load_defect_data()
 try:
-    sensor_data = preprocess_sensor_data(load_sensor_data())
-    defect_data = load_defect_data()
+    sensor_data, defect_data = load_data()
 except FileNotFoundError as e:
     print(f"Error loading sensor data: {e}")
     sensor_data = None
@@ -44,6 +43,8 @@ def health():
     return "API is running!"
 
 if __name__ == '__main__':
+    # Now safely do heavy things here
+    sensor_data, defect_data = load_data()  # if you have load_data function
     # Train model 
     # Only run this when manually running, not when importing in test
     train_model(sensor_data, defect_data)
